9e27c552588df088794ffe4fc4243e073996ac44
Eugeneayi <eugeneayi12@gmail.com>
2023-03-20 07:45:29 -0700

    file updated

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-char i;
-for (i = 'a' ; i <= 'z' ; i++);
+int i;
+for (i = 'a'; i <= 'z'; i++)
 putchar (i);
-for (i = 'A' ; i <= 'Z' ; i++);
+for (i = 'A'; i <= 'Z'; i++)
 putchar (i);
 putchar ('\n');
 return (0);
